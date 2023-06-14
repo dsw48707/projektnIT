c9fdb76f1326420d9e340a9517a605a853590783
dsw48707 <30624524+dsw48707@users.noreply.github.com>
2023-06-14 16:37:02 +0200

    Update konwerter.py

--- konwerter.py
+++ konwerter.py
@@ -1,76 +1,73 @@
-import argparse
+import sys
+import os
 import json
 import yaml
 import xml.etree.ElementTree as ET
-import xmltodict
-import xml.dom.minidom
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Konwerter plików')
-    parser.add_argument('input_file', type=str, help='Ścieżka do pliku wejściowego')
-    parser.add_argument('output_file', type=str, help='Ścieżka do pliku wyjściowego')
-    parser.add_argument('--format', type=str, choices=['json', 'yaml', 'xml'], help='Format pliku wyjściowego')
-    return parser.parse_args()
+def load_file(file_path):
+    _, file_extension = os.path.splitext(file_path)
 
-def load_json_file(file_path):
-    with open(file_path, 'r') as file:
-        try:
-            data = json.load(file)
-            return data
-        except json.JSONDecodeError:
-            print('Błąd: Niepoprawny format pliku JSON.')
-            return None
-
-def save_json_file(data, file_path):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4)
-
-def load_yaml_file(file_path):
-    with open(file_path, 'r') as file:
-        try:
-            data = yaml.safe_load(file)
-            return data
-        except yaml.YAMLError:
-            print('Błąd: Niepoprawny format pliku YAML.')
-            return None
-
-def save_yaml_file(data, file_path):
-    with open(file_path, 'w') as file:
-        yaml.dump(data, file)
+    if file_extension == '.xml':
+        return load_xml_file(file_path)
+    elif file_extension == '.json':
+        return load_json_file(file_path)
+    elif file_extension == '.yml' or file_extension == '.yaml':
+        return load_yaml_file(file_path)
+    else:
+        print("Unsupported file format")
+        return None
 
 def load_xml_file(file_path):
-    try:
-        tree = ET.parse(file_path)
-        root = tree.getroot()
-        return root
-    except ET.ParseError:
-        print('Błąd: Niepoprawny format pliku XML.')
-        return None
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    return root
+
+def load_json_file(file_path):
+    with open(file_path) as file:
+        data = json.load(file)
+    return data
+
+def load_yaml_file(file_path):
+    with open(file_path) as file:
+        data = yaml.safe_load(file)
+    return data
+
+def save_file(data, file_path):
+    _, file_extension = os.path.splitext(file_path)
+
+    if file_extension == '.xml':
+        save_xml_file(data, file_path)
+    elif file_extension == '.json':
+        save_json_file(data, file_path)
+    elif file_extension == '.yml' or file_extension == '.yaml':
+        save_yaml_file(data, file_path)
+    else:
+        print("Unsupported file format")
 
 def save_xml_file(root, file_path):
     tree = ET.ElementTree(root)
     tree.write(file_path)
 
-def convert_file(input_file, output_file, output_format):
-    if output_format == 'json':
-        data = load_json_file(input_file)
-        if data is not None:
-            save_json_file(data, output_file)
-            print('Plik skonwertowany do formatu JSON.')
-    elif output_format == 'yaml':
-        data = load_yaml_file(input_file)
-        if data is not None:
-            save_yaml_file(data, output_file)
-            print('Plik skonwertowany do formatu YAML.')
-    elif output_format == 'xml':
-        root = load_xml_file(input_file)
-        if root is not None:
-            save_xml_file(root, output_file)
-            print('Plik skonwertowany do formatu XML.')
-    else:
-        print('Błąd: Nieobsługiwany format pliku wyjściowego.')
+def save_json_file(data, file_path):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+def save_yaml_file(data, file_path):
+    with open(file_path, 'w') as file:
+        yaml.safe_dump(data, file)
+
+def convert_file(input_file_path, output_file_path):
+    data = load_file(input_file_path)
+    if data is not None:
+        save_file(data, output_file_path)
 
 if __name__ == '__main__':
-    args = parse_arguments()
-    convert_file(args.input_file, args.output_file, args.format)
+    if len(sys.argv) != 3:
+        print("Usage: program.exe pathFile1.x pathFile2.y")
+        sys.exit(1)
 
+
+    input_file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
+
+    convert_file(input_file_path, output_file_path)
